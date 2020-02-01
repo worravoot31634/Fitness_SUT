ce842835f2fb56d4c3b35e246f2a184088006ed6
worravoot31634 <55147927+worravoot31634@users.noreply.github.com>
2020-02-02 00:43:34 +0700

    update Video

--- updateVideo1.php
+++ updateVideo1.php
@@ -1,13 +1,11 @@
 <?php
-
+session_start();
 include 'connect.php';
 
 
-$time = $_POST['time'];
-$accountID = $_POST['accountID'];
 
 
-$sql = "UPDATE video SET video1 = " . $time . " WHERE accountID = " . $accountID;
+$sql="SELECT * FROM video WHERE accountID = " . $_SESSION['accountID'];
 
 $rs=$conn->query($sql); 
 
